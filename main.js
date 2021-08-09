869666878642f64b89c21b78e2b414659b26db51
FaiezWaseem <78906502+FaiezWaseem@users.noreply.github.com>
2021-08-09 11:47:02 +0500

    nothing

---
+++ main.js
@@ -0,0 +1,13 @@
+var qrcode = new QRCode("qrcode");
+
+function makeCode () {    
+  var elText = document.getElementById("text");
+  
+  if (!elText.value) {
+    alert("Input a text");
+    elText.focus();
+    return;
+  }
+  
+  qrcode.makeCode(elText.value);
+}
